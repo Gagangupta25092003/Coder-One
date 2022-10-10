79ab1e99d9107135c6a5fddc7939ccd56e922410
Gagangupta25092003 <95013939+Gagangupta25092003@users.noreply.github.com>
2022-10-11 00:43:31 +0530

    Add files via upload

---
+++ helper2.py
@@ -0,0 +1,438 @@
+########################
+###    Pathfinder    ###
+########################
+import numpy as np
+class Node:
+
+    def __init__(self, parent=None, location=None, action=None):
+        self.parent = parent
+        self.location = location
+        self.action = action
+
+        self.h = 0
+        self.g = 0
+        self.f = 0
+
+def get_path(node):
+    path = []
+
+    while node.parent:
+        path.append(node)
+        node = node.parent
+
+    return path
+
+def get_path_actions(path):
+
+    actions = []
+
+    for node in path:
+        actions.append(node.action)
+
+    return actions
+
+def astar(game_state, start, target):
+
+        print("----A* STAR----")
+        path = []
+
+        # add starting node to open list
+        open_list = [Node(None, start, None)]
+        closed_list = []
+
+        # exit the loop early if no path can be found
+        # (the target is likely blocked off)
+        max_loops = 500
+        counter = 0
+
+        # while lowest rank in OPEN is not the GOAL:
+        while len(open_list) > 0 and counter <= max_loops:
+
+            # find the node with the lowest rank
+            curr_node = open_list[0]
+            curr_index = 0
+
+            for index, node in enumerate(open_list):
+                if node.f < curr_node.f:
+                    curr_node = node
+                    curr_index = index
+
+            # check-`1222 ` if this node is the goal
+            if curr_node.location == target:
+                print(f"~~~~~~~FOUND TARGET~~~~~~~")
+                path = get_path(curr_node)
+                return path
+
+            # current = remove lowest rank item from OPEN
+            # add current to CLOSED
+            del open_list[curr_index]
+            closed_list.append(curr_node)
+
+            # get neighbors of current
+            neighbors = get_free_neighbors(game_state, curr_node.location)
+            neighbor_nodes = []
+            for neighbor in neighbors:
+                for location, action in neighbor.items():
+                    neighbor_nodes.append(Node(None, location, action))
+
+            #   for neighbors of current:
+            for neighbor in neighbor_nodes:
+                
+                # used for loop behavior
+                in_closed = False
+                in_open = False
+
+                # cost = g(current) + movementcost(current, neighbor)
+                cost = curr_node.g + 1
+
+                # if neighbor in OPEN and cost less than g(neighbor):
+                #   remove neighbor from OPEN, because new path is better
+                for index, node in enumerate(open_list):
+                    if neighbor.location == node.location and cost < neighbor.g:
+                        del open_list[index]
+                        in_open = True
+
+                # if neighbor in CLOSED and cost less than g(neighbor): ⁽²⁾
+                #   remove neighbor from CLOSED
+                for index, node in enumerate(closed_list):
+                    if neighbor.location == node.location and cost < neighbor.g: 
+                        del closed_list[index]
+                        in_closed = True
+
+                # if neighbor not in OPEN and neighbor not in CLOSED:
+                #   set g(neighbor) to cost
+                #   add neighbor to OPEN
+                #   set priority queue rank to g(neighbor) + h(neighbor)
+                #   set neighbor's parent to current
+                if not in_open and not in_closed:
+                    neighbor.g = cost
+                    open_list.append(neighbor)
+                    neighbor.h = manhattan_distance(neighbor.location, target)
+                    neighbor.f = neighbor.g + neighbor.h
+                    neighbor.parent = curr_node
+
+            counter += 1
+        print(f"---NO PATH FOUND---")
+
+def astar_b(game_state, start, target, game_map):
+
+    print("----A* STAR----")
+    path = []
+
+    # add starting node to open list
+    open_list = [Node(None, start, None)]
+    closed_list = []
+
+    # exit the loop early if no path can be found
+    # (the target is likely blocked off)
+    max_loops = 500
+    counter = 0
+
+    # while lowest rank in OPEN is not the GOAL:
+    while len(open_list) > 0 and counter <= max_loops:
+
+        # find the node with the lowest rank
+        curr_node = open_list[0]
+        curr_index = 0
+
+        for index, node in enumerate(open_list):
+            if node.f < curr_node.f:
+                curr_node = node
+                curr_index = index
+
+        # check-`1222 ` if this node is the goal
+        x,y = curr_node.location
+        if curr_node.location == target or game_map[y][x] == 0:
+            print(f"~~~~~~~FOUND TARGET~~~~~~~")
+            path = get_path(curr_node)
+                
+            return path
+
+        # current = remove lowest rank item from OPEN
+        # add current to CLOSED
+        del open_list[curr_index]
+        closed_list.append(curr_node)
+
+        # get neighbors of current
+        neighbors = get_free_neighbors(game_state, curr_node.location)
+        neighbor_nodes = []
+        for neighbor in neighbors:
+            for location, action in neighbor.items():
+                neighbor_nodes.append(Node(None, location, action))
+
+        #   for neighbors of current:
+        for neighbor in neighbor_nodes:
+                
+            # used for loop behavior
+            in_closed = False
+            in_open = False
+
+                # cost = g(current) + movementcost(current, neighbor)
+            cost = curr_node.g + 1
+
+            # if neighbor in OPEN and cost less than g(neighbor):
+            #   remove neighbor from OPEN, because new path is better
+            for index, node in enumerate(open_list):
+                if neighbor.location == node.location and cost < neighbor.g:
+                    del open_list[index]
+                    in_open = True
+
+            # if neighbor in CLOSED and cost less than g(neighbor): ⁽²⁾
+            #   remove neighbor from CLOSED
+            for index, node in enumerate(closed_list):
+                if neighbor.location == node.location and cost < neighbor.g: 
+                    del closed_list[index]
+                    in_closed = True
+
+            # if neighbor not in OPEN and neighbor not in CLOSED:
+            #   set g(neighbor) to cost
+            #   add neighbor to OPEN
+            #   set priority queue rank to g(neighbor) + h(neighbor)
+            #   set neighbor's parent to current
+            if not in_open and not in_closed:
+                neighbor.g = cost
+                open_list.append(neighbor)
+                neighbor.h = manhattan_distance(neighbor.location, target)
+                neighbor.f = neighbor.g + neighbor.h
+                neighbor.parent = curr_node
+
+        counter += 1
+    print(f"---NO PATH FOUND---")
+
+# given our current location, return only surrounding tiles that are free
+def get_free_neighbors(game_state, location):
+
+    x, y = location
+    neighbors = [{(x-1, y): 'l'}, {(x+1, y): 'r'}, {(x, y-1): 'd'}, {(x, y+1): 'u'}]
+    free_neighbors = []
+
+    for neighbor in neighbors:
+        for tile, direction in neighbor.items():    
+            if game_state.is_in_bounds(tile):
+                if game_state.is_occupied(tile):
+                     # check if this tile contains treasure or ammo
+                    if game_state.entity_at(tile) == 't' or game_state.entity_at(tile) == 'a':
+                        free_neighbors.append({tile:direction})
+                else:
+                    free_neighbors.append({tile:direction})
+
+    return free_neighbors
+
+# returns the manhattan distance between two tiles, calculated as:
+# 	|x1 - x2| + |y1 - y2|
+def manhattan_distance(start, end):
+
+    distance = abs(start[0] - end[0]) + abs(start[1] - end[1])
+
+    return distance
+
+# finds ammo, if any
+def get_ammo(game_state):
+
+    ammo = game_state.ammo
+
+    return ammo # return first ammo on the list
+
+# finds treasure, if any
+def get_treasure(game_state):
+
+    treasure = game_state.treasure
+
+    if treasure:
+        return treasure # return first treasure on the list
+
+def bomb_places(location):
+    x,y = location
+    l = [(x,y),(x+1,y),(x+2,y),(x-1,y),(x-2,y),(x,y+1),(x,y+2),(x,y-1),(x,y-2)]
+    return l
+
+def enemy_nearplaces(e_location,p_location,game_state):
+    x,y = e_location
+    location = []
+    if not game_state.is_occupied((x+1,y)):
+        location.append((x+1,y))
+    if not game_state.is_occupied((x-1,y)):
+        location.append((x-1,y))
+    if not game_state.is_occupied((x,y+1)):
+        location.append((x,y+1))
+    if not game_state.is_occupied((x,y-1)):
+        location.append((x,y-1))
+
+    for i in range(len(location)):
+        for j in range(i,len(location)):
+            if(manhattan_distance(location[i], p_location) > manhattan_distance(location[j], p_location)):
+                target = location[i]
+                location[i] = location[j]
+                location[j] = target
+
+    print(location)
+    return (location)
+
+
+
+def safe_place(game_state, p_location, game_map):
+    pos = []
+    rows = game_state.size[1]
+    cols = game_state.size[0]
+    for i in range(rows):
+        for j in range(cols):
+            if game_map[i][j] == 0 and manhattan_distance((j,i),p_location) == 1:
+                pos.append((i,j))
+
+    for i in range(rows):
+        for j in range(cols):
+            if game_map[i][j] == 0 and manhattan_distance((j,i),p_location) == 2:
+                pos.append((i,j))
+
+    for i in range(rows):
+        for j in range(cols):
+            if game_map[i][j] == 0 and manhattan_distance((j,i),p_location) == 3:
+                pos.append((i,j))
+
+    for i in range(rows):
+        for j in range(cols):
+            if game_map[i][j] == 0 and manhattan_distance((j,i),p_location) == 4:
+                pos.append((i,j))
+
+    for i in range(rows):
+        for j in range(cols):
+            if game_map[i][j] == 0 and manhattan_distance((j,i),p_location) == 5:
+                pos.append((i,j))
+
+    return pos
+
+def anyoption(p_location, game_map, game_state):
+    x,y = p_location
+    togo = []
+    pos =[]
+    if game_state.is_in_bounds((x+1,y)):
+        pos.append((x+1,y))
+    if game_state.is_in_bounds((x-1,y)):
+        pos.append((x-1,y))
+    if game_state.is_in_bounds((x,y+1)):
+        pos.append((x,y+1))
+    if game_state.is_in_bounds((x,y-1)):
+        pos.append((x,y-1))
+    
+    for i in pos:
+        x,y = i
+        if game_map[y][x] == 0:
+            togo.append(i)
+
+    return togo
+
+def nextPosition(act,p_location):
+    x,y = p_location
+    if act == 'u':
+        y = y+1
+    if act == 'd':
+        y = y-1
+    if act == 'r':
+        x = x+1
+    if act == 'l':
+        x = x-1
+    l = (x,y)
+    return l
+
+def isnearenemy(e_location,p_location):
+    x,y = e_location
+    l = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
+
+    if p_location in l:
+        return True
+    else:
+        return False
+
+def get_unsafe_places(bombs,gamemap,game_state):
+    for i in bombs:
+        x,y = i
+        if game_state.is_in_bounds((x,y-2)):
+            y,x = i
+            if gamemap[x-1][y] != 2 and gamemap[x-1][y] != 3 and gamemap[x-2][y] != 2 and gamemap[x-2][y] != 3:
+                gamemap[x-2][y] = -1
+        x,y = i
+        if game_state.is_in_bounds((x,y+2)):
+            y,x = i
+            if gamemap[x+1][y] != 2 and gamemap[x+1][y] != 3 and gamemap[x+2][y] != 2 and gamemap[x+2][y] != 3:
+                gamemap[x+2][y] = -1
+        x,y = i
+        if game_state.is_in_bounds((x-2,y)):
+            y,x = i
+            if gamemap[x][y-1] != 2 and gamemap[x][y-1] != 3 and gamemap[x][y-2] != 2 and gamemap[x][y-2] != 3:
+                gamemap[x][y-2] = -1
+        x,y = i
+        if game_state.is_in_bounds((x+2,y)):
+            y,x = i
+            if gamemap[x][y+1] != 2 and gamemap[x][y+1] != 3 and gamemap[x][y+2] != 2 and gamemap[x][y+2] != 3:
+                gamemap[x][y+2] = -1
+
+        x,y = i
+        if game_state.is_in_bounds((x,y-1)):
+            y,x = i
+            if gamemap[x-1][y] != 2 and gamemap[x-1][y] != 3:
+                gamemap[x-1][y] = -1
+        x,y = i
+        if game_state.is_in_bounds((x,y+1)):
+            y,x = i
+            if gamemap[x+1][y] != 2 and gamemap[x+1][y] != 3 :
+                gamemap[x+1][y] = -1
+        x,y = i
+        if game_state.is_in_bounds((x-1,y)):
+            y,x = i
+            if gamemap[x][y-1] != 2 and gamemap[x][y-1] != 3 :
+                gamemap[x][y-1] = -1
+        x,y = i
+        if game_state.is_in_bounds((x+1,y)):
+            y,x = i
+            if gamemap[x][y+1] != 2 and gamemap[x][y+1] != 3 :
+                gamemap[x][y+1] = -1
+        x,y = i
+        gamemap[y][x] = -1
+
+    return gamemap
+        
+
+
+def print_map(self, game_state):
+        cols = game_state.size[0]
+        rows = game_state.size[1]
+        game_map = np.zeros((rows, cols))
+        for x in range(cols):
+            for y in range(rows):
+                entity = str(game_state.entity_at((x,y)))
+                if entity == 'None' or entity == 't' or entity == '1' or entity == 'a':
+                    game_map[y][x] = 0
+                elif entity == '0':
+                    game_map[y][x] = 1
+                elif entity == 'sb':
+                    game_map[y][x] = 2
+                else:
+                    game_map[y][x] = 3
+                     
+        return game_map
+    
+def move(pf_location,pi_location):
+    print(f"pf location : {pf_location}")
+    x,y = pi_location
+    X,Y = pf_location
+    if X-x == 1:
+        return 'l'
+    elif X-x == -1:
+        return 'r'
+    elif Y-y == 1:
+        return 'd'
+    else:
+        return 'u'
+
+def nmove(action,pi_location):
+    x,y = pi_location
+    if action == 'l':
+        return (x-1,y)
+    elif action == 'r':
+        return (x+1,y)
+    elif action == 'u':
+        return (x,y+1)
+    elif action == 'd':
+        return (x,y-1)
+    else:
+        return (x,y)
---
+++ my_agent.py
@@ -0,0 +1,205 @@
+from .helpers import *
+import numpy as np
+
+class Agent:
+
+    def __init__(self):
+        '''
+        Place any initialization code for your agent here (if any)
+        ''' 
+        self.pending_actions = []
+        self.pending_actions_bombs = []
+        self.bombs_places=[]
+        self.bombs_ticks=[]
+        self.actions_taken= []        
+        self.treasure=[]
+        self.nextposition = ()      
+
+    def next_move(self, game_state, player_state):
+
+        
+        player_ammo = player_state.ammo
+        player_location = player_state.location
+        enemy_location = game_state.opponents(0)[0]
+        ammo = get_ammo(game_state)
+        trs = get_treasure(game_state)
+        action = ''
+        tick_number = game_state.tick_number
+        bombs = game_state.bombs
+        self.pending_actions = []
+
+        print()
+        print(f"tick: {game_state.tick_number}")
+        print(player_location)
+        print(enemy_location)
+        print()
+        print(f"actions taken : {self.actions_taken}")
+        print(f"next position : {self.nextposition}")
+        
+
+        if tick_number == 0:
+            self.nextposition = player_location
+        
+        else:
+            if self.actions_taken:
+                self.nextposition = nmove(self.actions_taken.pop() , self.nextposition )
+                if self.nextposition != player_location:
+                    
+                    return ""
+            
+        X,Y = player_location
+        print(f"actions taken : {self.actions_taken}")
+        print(f"next position : {self.nextposition}")
+        print(f"bombs places {self.bombs_places}")
+        print(f"bombs ticks {self.bombs_ticks}")
+
+        for i in range(len(self.bombs_places)):
+            if len(self.bombs_places) > i :
+                if tick_number > self.bombs_ticks[i] + 35:
+                    self.bombs_ticks.remove(self.bombs_ticks[i])
+                    self.bombs_places.remove(self.bombs_places[i])
+
+        
+        for i in bombs:
+            if i not in self.bombs_places:
+                self.bombs_places.append(i)
+                self.bombs_ticks.append(tick_number)
+
+        game_map = print_map(self, game_state)
+        
+        game_map = get_unsafe_places(self.bombs_places,game_map,game_state)
+        print(str(self.pending_actions)+ "  pending actions")
+        print(str(self.pending_actions_bombs)+ "  pending actions under escaping bombs") 
+        
+        
+
+        if bombs:
+            if game_map[Y][X] != 0:
+
+                if len(self.pending_actions_bombs) > 0:
+                    
+                    print(str(self.pending_actions_bombs)+ "  pending actions under escaping bomb")
+                    action = self.pending_actions_bombs.pop()
+                    print(f"Action under escaping bomb: {action}")
+                
+                else:                    
+                    pos = safe_place(game_state, player_location ,game_map)
+
+                    if pos:
+                        for i in pos:
+                            path = astar_b(game_state, player_location, i,game_map)
+                            if path:
+                                
+                                print(f"target : {i}")
+                                print(f"player location : {player_location}")
+                                actions = get_path_actions(path)
+                                for i in actions:
+                                    self.pending_actions_bombs.append(i)
+                                
+                                print(str(self.pending_actions_bombs)+ "  pending actions under escaping bomb")
+                                action = self.pending_actions_bombs.pop()
+                                print(f"Action under escaping bomb: {action}")
+                                if action != "":
+                                    break
+                                
+
+                if action != '' and not game_state.is_occupied(nextPosition(action,player_location)):
+                    self.actions_taken.append(action)
+                    return action
+                elif action != '':
+                    self.pending_actions_bombs.append(action)
+                    
+                
+
+        togo = anyoption(player_location, game_map, game_state)
+        print(togo)
+        if togo :
+
+            print("Player has safe positions to go")
+            print(togo)
+
+            if ammo :     
+
+                    for i in ammo:
+                
+                        print("Player searching for ammo path")
+
+                        path = astar(game_state, player_location, i)
+
+                        if path:
+                            actions = get_path_actions(path)
+                            for i in actions:
+                                self.pending_actions.append(i)
+                            print(str(self.pending_actions)+ "  pending actions under ammo ")
+                            action = self.pending_actions.pop()
+                            break
+                        
+                        
+
+            elif trs :                  
+                    for i in trs:
+
+                        path = astar(game_state, player_location, i)
+                        if path:
+                            actions = get_path_actions(path)
+                            for i in actions:
+                                self.pending_actions.append(i)
+                            print(str(self.pending_actions)+ "  pending actions under")
+                            action = self.pending_actions.pop()
+                            break
+
+            f_pos = nmove(action, player_location)
+
+            if f_pos in togo:
+                    print(f"Action under taking ammo/treasure: {action}")
+                    self.actions_taken.append(action)
+                    return action
+            elif action != '':
+                self.pending_actions.append(action)
+            
+
+            action = ''
+
+            if player_ammo >0 :              
+
+                print("Player is going towards enemy")
+                if isnearenemy(enemy_location,player_location) and game_state.tick_number == tick_number:
+                    action = 'p'
+                    print(f"Action under reaching enemy: {action}")
+                    return action             
+                
+                else:
+                    print("********************************")
+                    tar = enemy_nearplaces(enemy_location,player_location,game_state)
+                    if tar:
+                        for i in tar:
+                            path = astar(game_state,player_location,i)
+                            if path:
+                                actions = get_path_actions(path)
+                                for i in actions:
+                                    self.pending_actions.append(i)
+                                
+                                print(str(self.pending_actions)+ "  pending actions under reaching enemy ")
+
+                                action = self.pending_actions.pop()
+                                break
+    
+
+                f_pos = nextPosition(action,player_location)
+                
+
+                if f_pos in togo:
+                    print(f"Action under reaching enemy: {action}")
+                    self.actions_taken.append(action)
+                    return action
+
+                elif action != '':
+                    self.pending_actions.append(action)
+
+                                                                                                                                                                                  
+            self.actions_taken.append('')
+            return ''  
+
+        else:
+            self.actions_taken.append('')
+            return ''
